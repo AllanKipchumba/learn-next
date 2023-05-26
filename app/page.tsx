21df35511d363f41721f7c9944672dd3e3d781f7
allan <allankipchumba76@gmail.com>
2023-05-26 17:03:44 +0300

    completed data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-row items-center justify-between p-24">
       <h1>Hello world</h1>
       <Link href="/about">Go to about page</Link>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </main>
   );
 }
